2a104fe52758f16a68b9abf2b8f7754ab8d8dcec
visha <visha@LAPTOP-8COK7CO2>
2020-10-21 07:51:04 +0530

    variables  java program

---
+++ Variables & Datatypes/src/variables.java
@@ -0,0 +1,28 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declaring variables
+				int marks1;
+				int marks2;
+				
+				
+				// Assigning values & initialization of variables
+				
+				marks1 = 75;
+				marks2 = 85;
+				
+				
+				// declaration of variables in single line
+				String student1 = "Vishal";
+				String student2 = "Mayuri";
+				
+				System.out.println(marks1);
+				System.out.println(student2);;
+
+
+	}
+
+}
