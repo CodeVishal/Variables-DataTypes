4269567de428c21596c5c18798643e4cb31d0042
visha <visha@LAPTOP-8COK7CO2>
2020-10-21 08:05:30 +0530

    variablesUpdate program

---
+++ Variables & Datatypes/src/variablesUpdate.java
@@ -0,0 +1,30 @@
+
+public class variablesUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declaring & Initialization of variable in one line 
+		
+				int distanceAdam = 0 ,distanceLucy = 0;
+				
+				// Printing out initial values of these variables
+				
+				System.out.println("initial value" + distanceAdam);
+				System.out.println("initial value" + distanceLucy);
+				
+				
+				// updating these variables
+				distanceAdam = distanceAdam + 20;
+				
+				// updating variable by overwriting 
+				distanceLucy = 15;
+				
+				
+				// printing out final updated values
+				System.out.println("updated value" + distanceAdam);
+				System.out.println("updated value" + distanceLucy);
+
+	}
+
+}
