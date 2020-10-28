0d154240d0dca9be2215a17b7821fa640ce3f44c
visha <visha@LAPTOP-8COK7CO2>
2020-10-28 07:18:47 +0530

    Commit Airthmatic Operations & Data types

---
+++ Variables & Datatypes/src/DatatypesAssignment.java
@@ -0,0 +1,25 @@
+/*
+ * Description
+ * Consider that you want to store distance between two points in variables.
+   The distance given is 2.367 kms.
+   Initialize a variable named “distance” to store the distance in kilometres.
+   Initialize a variable named “distanceInMetres” to store the distance in metres.
+Note: Remember to use appropriate data types to store the distances.
+ */
+public class DatatypesAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Initialize variables here
+		
+		float distance;
+		distance = 2.367f;
+		int distanceInMeters;
+		distanceInMeters = 2367;
+		
+		System.out.println("Distance in Km=" + distanceInMeters);
+		System.out.println("Distance in meters=" + distanceInMeters);
+	}
+
+}
