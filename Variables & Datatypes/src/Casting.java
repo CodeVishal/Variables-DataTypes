445daa61f9780021de426537a8b86e0be82e3ee5
visha <visha@LAPTOP-8COK7CO2>
2020-10-29 08:29:48 +0530

    Casting in java

---
+++ Variables & Datatypes/src/Casting.java
@@ -0,0 +1,22 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int marks1 = 20;
+		int marks2 = 40;
+		int marks3 = 50;
+		
+		double average = ((double)marks1 + (double)marks2 + (double)marks3)/3;
+		
+		System.out.println(average);
+		
+		// Another example 
+		char x = 'a';
+		int value = (int)x;
+		System.out.println(value);
+
+	}
+
+}
